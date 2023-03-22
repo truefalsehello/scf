8b08a0143ae08935d92326003d0d66eca76cd70e
yu.dongliang <18588496441@163.com>
2023-03-22 21:17:14 +0800

    add ../examples/print.c

---
+++ examples/print.c
@@ -0,0 +1,17 @@
+
+int printf(const char* fmt, ...);
+
+int main()
+{
+	char buf[1024];
+
+	float   f   = 2.71828;
+	double  d   = -3.1415926;
+	int64_t i64 = -255;
+
+	int ret = printf("i: %d, ld: %ld,  x: %x, x: %#lx, p: %p, s: %s, f: %f, d: %lf\n",
+			100, i64, 254, 253, buf, "hello", f, -3.14);
+
+	return ret;
+}
+
